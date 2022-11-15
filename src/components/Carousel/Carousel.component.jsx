e81f46f550601cc482157143985142af1f4a7d6f
clementleroux-dev <clement.lerouxvi2@gmail.com>
2022-11-15 10:07:15 +0100

    fix svg icons

--- src/components/Carousel/Carousel.component.jsx
+++ src/components/Carousel/Carousel.component.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Carousel.scss";
-import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 //initialize state of current picture to 0
 const Carousel = ({ pictures }) => {
@@ -22,19 +21,27 @@ const Carousel = ({ pictures }) => {
         className="carousel__img"
         alt="accommodation"
       ></img>
-      <MdChevronLeft
+      <svg
         onClick={prev}
         className={`carousel__chevron left ${length <= 1 ? "hidden" : ""}`}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
       >
+        <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
         Prev
-      </MdChevronLeft>
-      <MdChevronRight
+      </svg>
+
+      <svg
         onClick={next}
         className={`carousel__chevron right ${length <= 1 ? "hidden" : ""}`}
         hidden={length > 1 ? "hidden" : ""}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
       >
+        {" "}
+        <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
         Next
-      </MdChevronRight>
+      </svg>
       <span className="carousel__count">
         {current + 1}/{length}
       </span>
--- src/components/Collapse/Collapse.component.jsx
+++ src/components/Collapse/Collapse.component.jsx
@@ -1,6 +1,5 @@
 import "./Collapse.scss";
 import { useState } from "react";
-import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 
 //set open on false, and set toggle
 const Collapse = ({ title, children }) => {
@@ -15,9 +14,21 @@ const Collapse = ({ title, children }) => {
         <h2>{title}</h2>
         <span>
           {open ? (
-            <BiChevronUp className="chevron" />
+            <svg
+              className="chevron"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+            </svg>
           ) : (
-            <BiChevronDown className="chevron" />
+            <svg
+              className="chevron"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+            </svg>
           )}
         </span>
       </div>
